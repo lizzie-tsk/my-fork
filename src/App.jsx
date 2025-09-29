bcd5830cbe617eda1a8e9623543be98aa99e330b
lizzie-tsk <lizzietskhovrebadze@LT-Mac.local>
2025-09-29 19:16:12 -0400

    Changed title and added a small lol

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+
+
+
 function App() {
     const [count, setCount] = useState(0);
-    
+
     return (
         <>
             <div>
@@ -20,7 +23,8 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Lizzie Tskhovrebadze</h1>
+            <h4>lol</h4>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
